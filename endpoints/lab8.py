8fd3167a638a99f04771d4785212262613641ab6
Vladislav Antonov <lolik1806@gmail.com>
2022-11-20 18:22:18 +0300

    lab 8

---
+++ endpoints/lab8.py
@@ -0,0 +1,74 @@
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+
+lab8Router = APIRouter()
+
+
+@lab8Router.get("/get-rsa-encrypt")
+def vigenere_encrypt(file_name: str, is_encoding: bool = True) -> JSONResponse:
+    file = open(f"files/labs/{file_name}", "rb")
+    text = file.read().decode("utf-8")
+    result_text = ""
+
+    p = 53
+    q = 59
+
+    n = p * q
+    f = (p - 1) * (q - 1)
+    e = 3
+    d = 0
+
+    # for i in range(2, n - 1):
+    #     if n // i != 0:
+    #         e = i
+    #         break
+
+    i = 1
+    while True:
+        if (i * e) % f == 1:
+            d = i
+            break
+        else:
+            i += 1
+
+    if is_encoding:
+        result_text = encrypt(text, n, e)
+
+        lab8File = open("files/results/lab8-result.txt", mode="w", encoding="utf-8")
+        lab8File.write(result_text)
+        lab8File.close()
+    else:
+        result_text = decrypt(text, n, d)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "text": str(text),
+            "result": str(result_text)
+        }
+    )
+
+
+def encrypt(text: str, n: int, e: int) -> str:
+    result: list[str] = list()
+
+    for char in text:
+        c = ord(char) ** e % n
+
+        result.append(str(c))
+
+    return " ".join(result)
+
+
+def decrypt(text: str, n: int, d: int) -> str:
+    # Массив закодировнных символов
+    chars: list[str] = text.split()
+
+    result: list[str] = list()
+
+    for char in chars:
+        c = int(char) ** d % n
+
+        result.append(chr(c))
+
+    return "".join(result)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from endpoints import common, lab1, lab2, lab3, lab4, lab5, lab6, lab7
+from endpoints import common, lab1, lab2, lab3, lab4, lab5, lab6, lab7, lab8
 
 app = FastAPI(title="Information Security")
 
@@ -21,6 +21,7 @@ app.include_router(lab4.lab4Route, prefix='/lab4', tags=['lab4'])
 app.include_router(lab5.lab5Router, prefix='/lab5', tags=['lab5'])
 app.include_router(lab6.lab6Router, prefix='/lab6', tags=['lab6'])
 app.include_router(lab7.lab7Router, prefix='/lab7', tags=['lab7'])
+app.include_router(lab8.lab8Router, prefix='/lab8', tags=['lab8'])
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=5000, log_level="info", reload=True)
